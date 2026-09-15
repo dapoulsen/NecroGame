6643336afde8eb977fa8e08b08a7312618c23c8c
dapoulsen <dapoulsen14@gmail.com>
2026-09-15 12:17:11 +0200

    trying input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,8 @@ public class PlayerController : MonoBehaviour
 {
     // Movement tuning
     public float movementSpeed = 0.01f;
-    //Input System action exposed in Inspector for binding (WASD keys)
-    public InputAction MoveAction;
-    private Vector2 moveInput;
+    private Vector2 _moveInput;
+    public InputAction MoveAction; 
 
     //Called when player becomes enabled or active
     void OnEnable() {
@@ -17,16 +16,16 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Read the 2D vector from the MoveAction
-       moveInput = MoveAction.ReadValue<Vector2>(); 
+       _moveInput = MoveAction.ReadValue<Vector2>(); 
 
-       Vector2 position = (Vector2)transform.position + moveInput * movementSpeed;
+       Vector2 position = (Vector2)transform.position + _moveInput * movementSpeed;
 
        transform.position = position;
     }
